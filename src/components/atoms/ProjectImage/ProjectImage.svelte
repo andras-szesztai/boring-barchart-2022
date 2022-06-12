2e11c0fee534b9b84a7a6a5758c0077b030c2f4a
András Szesztai <andras.szesztai@nielsen.com>
2022-06-12 17:50:56 +0200

    fix: add scrset to image

--- src/components/atoms/ProjectImage/ProjectImage.svelte
+++ src/components/atoms/ProjectImage/ProjectImage.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <div class="container">
-  <img {src} alt="" />
+  <img
+    {src}
+    alt=""
+    srcset={`${src}?w=400&h=200&fit=crop 400w, ${src}?w=600&h=300&fit=crop 600w, ${src}?w=800&h=400&fit=crop 800w, ${src}?w=1000&h=500&fit=crop 1000w, ${src}?w=1200&h=600&fit=crop 1200w`}
+  />
 </div>
 
 <style>
